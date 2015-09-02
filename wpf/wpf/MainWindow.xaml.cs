8f7ffcc0af2051ef2264fa54e37721fee90078d4
renho <rho_i_xx@163.com>
2015-09-02 23:28:35 +0800

    20150902_renho_commit

---
+++ wpf/wpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public String tValue = "123";
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox.SetBinding(TextBox.TextProperty, new Binding(".") { Source = tValue });
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            String s = tValue;
+            MessageBox.Show("refresh:" + s);
+            tValue = "renho";
+        }
+    }
+}
